26dcc626e405cae3aa9fbe79f9037698fb137afc
barkinsarikartal <barkinsarikartal51@gmail.com>
2024-02-03 19:22:05 +0300

    comparing valid card ID and read card ID fixed

--- UID_Okuma/UID_Okuma.ino
+++ UID_Okuma/UID_Okuma.ino
@@ -8,18 +8,14 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 MFRC522DriverPinSimple ss_pin(10);
-
 MFRC522DriverSPI driver{ss_pin};
-
 MFRC522 reader{driver};
 
-String validcard = "c3 31 c5 06";
-
+String validCardID = " C3 31 C5 06";
 String okunanKartID = "";
 
 
 void setup() {
-  
   pinMode(3, OUTPUT);
   pinMode(5, OUTPUT);
 
@@ -36,94 +32,91 @@ void setup() {
   lcd.init(); 
   lcd.backlight();
   lcd.clear();
-  lcd.print("Kart Okutun");
-  lcd.setCursor(11,1);
-  lcd.print("13:15");
+  lcd.setCursor(6,0);
+  lcd.print("KART");
+  lcd.setCursor(5,1);
+  lcd.print("OKUTUN");
 }
 
 
 void loop() {
-
   if (reader.PICC_IsNewCardPresent() && reader.PICC_ReadCardSerial()) {
-      Serial.print(F("Reader "));
-      static uint8_t i = 0;
-      i++;
-      Serial.print(i);
-      
-      Serial.print(F(": Card UID:"));
-      MFRC522Debug::PrintUID(Serial, reader.uid);
-      Serial.println();
+    Serial.print(F("Card UID: "));
+    MFRC522Debug::PrintUID(Serial, reader.uid);
+    Serial.println();
 
-      okunanKartID = "";
+    okunanKartID = "";
 
-      for (byte i = 0; i < reader.uid.size; i++) {
-        okunanKartID += String(reader.uid.uidByte[i] < 0x10 ? " 0" : " ");
-        okunanKartID += String(reader.uid.uidByte[i], HEX);
-      }
-
-      Serial.print(F("PICC type: "));
-      MFRC522::PICC_Type piccType = reader.PICC_GetType(reader.uid.sak);
-      Serial.println(MFRC522Debug::PICC_GetTypeName(piccType));
-
-      lcd.clear();
-      lcd.print("Kart ID:");
-      lcd.setCursor(0,1);
-      MFRC522Debug::PrintUID(lcd, reader.uid);
-
-      
-      Serial.print(validcard);
-      Serial.println("hafızadaki kart: ");
-      Serial.print(okunanKartID);
-      Serial.print("okunan kart: ");
-
-      if(okunanKartID.equals(validcard)){
-          lcd.clear();
-          lcd.print("Kapi Aciliyor");
-          buzzerValid();
-          digitalWrite(5, HIGH);
-          delay(3000);
-          digitalWrite(5, LOW);
-      }
-      else if(okunanKartID != validcard){
-          lcd.clear();
-          lcd.print("Yetkisiz Kart");
-          buzzerDecline();
-      }
-
-      okunanKartID = "";
-      
-      lcd.clear();
-      lcd.print("Kart Okutun");
-      lcd.setCursor(11,1);
-      lcd.print("13:15");
-      
-      reader.PICC_HaltA();
-
-      reader.PCD_StopCrypto1();
+    for (byte i = 0; i < reader.uid.size; i++) {
+      okunanKartID += String(reader.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      okunanKartID += String(reader.uid.uidByte[i], HEX);
     }
-  }
 
-  void buzzerValid(){
-      digitalWrite(3, HIGH);
-      delay(200);
-      digitalWrite(3, LOW);
-      delay(100);
-      digitalWrite(3, HIGH);
-      delay(200);
-      digitalWrite(3, LOW);
+    okunanKartID.toUpperCase();
+
+    Serial.print(F("PICC type: "));
+    MFRC522::PICC_Type piccType = reader.PICC_GetType(reader.uid.sak);
+    Serial.println(MFRC522Debug::PICC_GetTypeName(piccType));
+
+    Serial.print(validCardID);
+    Serial.println(" hafızadaki kart");
+    Serial.print(okunanKartID);
+    Serial.println(" okunan kart");
+
+    if(okunanKartID == validCardID){
+      validCard();
+    }
+    else if(okunanKartID != validCardID){
+      declinedCard();
+    }
+
+    okunanKartID = "";
+      
+    lcd.clear();
+    lcd.setCursor(6,0);
+    lcd.print("KART");
+    lcd.setCursor(5,1);
+    lcd.print("OKUTUN");
+      
+    reader.PICC_HaltA();
+    reader.PCD_StopCrypto1();
   }
+}
+
+void validCard(){
+  lcd.clear();
+  lcd.setCursor(6,0);
+  lcd.print("KAPI");
+  lcd.setCursor(4,1);
+  lcd.print("ACILIYOR");
+  digitalWrite(3, HIGH);
+  delay(50);
+  digitalWrite(3, LOW);
+  delay(20);
+  digitalWrite(3, HIGH);
+  delay(50);
+  digitalWrite(3, LOW);
+  digitalWrite(5, HIGH);
+  delay(3000);
+  digitalWrite(5, LOW);
+}
   
-  void buzzerDecline(){
-      digitalWrite(3, HIGH);
-      delay(600);
-      digitalWrite(3, LOW);
-      delay(100);
-      digitalWrite(3, HIGH);
-      delay(600);
-      digitalWrite(3, LOW);
-      delay(100);
-      digitalWrite(3, HIGH);
-      delay(600);
-      digitalWrite(3, LOW);
-      delay(100);
-  }
+void declinedCard(){
+  lcd.clear();
+  lcd.setCursor(4,0);
+  lcd.print("YETKISIZ");
+  lcd.setCursor(6,1);
+  lcd.print("KART");
+  analogWrite(3, 155);
+  delay(600);
+  digitalWrite(3, LOW);
+  delay(100);
+  analogWrite(3, 155);
+  delay(600);
+  digitalWrite(3, LOW);
+  delay(100);
+  analogWrite(3, 155);
+  delay(600);
+  digitalWrite(3, LOW);
+  delay(100);
+}
